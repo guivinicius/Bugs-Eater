d086892c7e4a0b028e2fb556626dfc300a9ccf08
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2011-09-12 18:17:42 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bugseater::Application.config.secret_token = '966c5c61718a7ee83d9af1956cc5220679e544990a0cf44043b449e803727760c9df6f15398e6763b0427b09be0a065a273f11cd02b9b338614c081c8f0e7d86'
