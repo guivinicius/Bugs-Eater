6c084794c5afcba111cf2980d9e059a4eae13691
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2011-09-13 01:02:03 -0300

    Arrumando menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function(){
+  $("body").bind("click", function (e) {
+      $('.dropdown-toggle, .menu').parent("li").removeClass("open");
+    });
+    $(".dropdown-toggle, .menu").click(function (e) {
+      var $li = $(this).parent("li").toggleClass('open');
+      return false;
+  });  
+});
