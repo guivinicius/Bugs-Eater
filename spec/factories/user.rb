47302c3f2d28df4314be5f744250b9132d08d614
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2011-09-12 23:42:38 -0300

    Adicionando factory de user

---
+++ spec/factories/user.rb
@@ -0,0 +1,8 @@
+Factory.define :user do |user|
+   
+  user.name "Guilherme Vinicius Moreira"
+  user.email "gui.vinicius@gmail.com"
+  user.username "guivinicius"
+  ticket.password "123456"
+
+end
