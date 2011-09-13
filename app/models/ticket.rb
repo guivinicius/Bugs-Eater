c9ac44b3907ef9f5fb7928b0044eb22d6f1437db
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2011-09-13 00:00:47 -0300

    Adicionando o select de status

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,11 +1,16 @@
+# encoding: utf-8
+
 class Ticket
   include Mongoid::Document
   include Mongoid::Timestamps
+
+  STATUS = ['Aberto', 'Trabalhando', 'Precisa de Mais Informação', 'Fechado']
 
   embeds_many :comments
 
   field :name
   field :description
+  field :status, :default => "Aberto"
 
   validates :name, :description, :presence => true
   
